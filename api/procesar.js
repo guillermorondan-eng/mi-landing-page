6ce10b58ca626ae68f28f85774b25a6a0eeb30c5
guillermorondan-eng <guillermo.rondan@gmail.com>
2026-05-29 20:10:50 -0300

    Update procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -30,8 +30,7 @@ module.exports = async function handler(req, res) {
             }
 
            const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-           const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
+           const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
             const result = await model.generateContent([
                 {
                     inlineData: {
