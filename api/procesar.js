0be9f4f5bc135107c2a73c4b090af646daf5c841
guillermorondan-eng <guillermo.rondan@gmail.com>
2026-05-29 18:57:27 -0300

    Update procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -29,7 +29,7 @@ module.exports = async function handler(req, res) {
                 throw new Error("La variable de entorno GEMINI_API_KEY no está configurada.");
             }
 
-            const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+           const genAI = new GoogleGenerativeAI("AIzaSyCys3zgiN3izn7PiOoI-3wvAiwNajaTFaw");
             const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
             const result = await model.generateContent([
